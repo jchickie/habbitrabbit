cd3618809d1ae8bcd71e93e46b117b4650cc0c10
jchickie <jchickie@uncc.edu>
2019-12-01 04:48:23 -0500

    update and style habit breaking page

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-$( document ).ready(function() {
-    $(".minus-button").click(function() {
-        // $(this).remove($(this).parents(".row"));
-        console.log($(this));
-        $(this).closest(".row").remove();
-    })
+$(document).ready(function() {
+  $(".minus-button").click(function() {
+    // $(this).remove($(this).parents(".row"));
+    console.log($(this));
+    $(this)
+      .closest(".row")
+      .remove();
+  });
 
+  $(".delete-habit-btn").click(function() {
+    // $(this).remove($(this).parents(".row"));
+    console.log($(this));
+    if (confirm("Are you sure you want to delete this?")) {
+      $(this)
+        .closest(".habit-card")
+        .remove();
+    }
+  });
 });
